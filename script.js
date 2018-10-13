9a9c55e96b2f9e335bec08ba08f14a2a6000c820
Hing Tang <htang@levi.com>
2018-10-13 15:35:54 -0700

    add game options, number coordinate,

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-loadGame = {
+// TODO
+// choose 1 or 2 player
+// start game
+// winner logic
+// shoot missle, sink ship
+// player turns
+// cpu random placement
+// cpu ramdom fire
+
+battleShipt = {
 	init: function () {
 		this.cacheDom();
-		this.loadProduct();
+		this.buildBoardGame();
 	},
 	// caching DOM improve performance
 	cacheDom: function () {
 		this.gridContainer = document.querySelector("#game-board");
 		this.dashboard = this.gridContainer.querySelector(".player-dashboard");
 		this.tabs = this.gridContainer.querySelector(".nav-tabs");
+		this.gameOptions = this.gridContainer.querySelector(".select-player");
+		this.startGameBtn = this.gridContainer.querySelector("#start-game");
 		this.playerOneShips = [];
 		this.playerTwoShips = [];
 		this.playerTurn = '';
+		this.numOfPlayer = '';
+		this.gameStarted = false;
 	},
 	bindEvent: function () {
 		this.gridContainer.addEventListener("click", this.setShip.bind(this));
 		this.tabs.addEventListener("click", this.setPlayer.bind(this));
-
-		$('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-
+		this.gameOptions.addEventListener("click", this.selectGameOptions.bind(this));
+		this.startGameBtn.addEventListener("click", this.startGame.bind(this));
 	},
 	createElement: (el, classLists) => {
 		const element = document.createElement(el)
@@ -29,14 +38,28 @@ loadGame = {
 		}
 		return element;
 	},
-	loadProduct: function () {
+	selectGameOptions: (e) => {
+		this.numOfPlayer = e.target.children[0].id;
+		console.log(this.numOfPlayer);
+	},
+	startGame: (e) => {
+		e.stopPropagation();
+		console.log(Boolean(this.numOfPlayer))
+		if ( !Boolean(this.numOfPlayer)) {
+			console.log('please select number of player first. ')
+		} else {
+			console.log( 'start game now!! ' )
+			// this.buildBoardGame();
+		}
+	},
+	buildBoardGame: function () {
 		const numberOfPosition = 120;
 		const positionInColumn = 15;
 		let positionCount = 0;
 		let column = 0;
 		let row = 0;
-		let playerBoard = this.gridContainer.querySelector('.player1-pad');
-		// let playerTwoBoard = this.gridContainer.querySelector('.player2-pad')
+		let playerBoard = this.gridContainer.querySelector('.player1-board');
+		// let playerTwoBoard = this.gridContainer.querySelector('.player2-board')
 
 		for (let i = 0; i < numberOfPosition; i++) {
 			positionCount / positionInColumn === row ? row++ : row;
@@ -44,60 +67,65 @@ loadGame = {
 			positionCount++;
 
 			let placementContainer = this.createElement('div', ['position']);
+			placementContainer.setAttribute("data-coord-number", positionCount );
 			placementContainer.setAttribute("data-coordinate", `${row}, ${column}`);
 			playerBoard.appendChild(placementContainer);
 		}
-		this.bindEvent( )
+		this.bindEvent()
 	},
-	setPlayer: function(e) {
-		console.log(this);
-		console.log(e);
+	setPlayer: function (e) {
+		e.preventDefault()
+		battleShipt.playerTurn = e.target.parentElement.innerText;
+		console.log(this.playerTurn)
 	},
 	matchCoordinate: (arr, value) => {
 		return arr.some(function (item) {
 			return value === item;
 		});
 	},
-	getCoordinate: e => {
+	getCoordinate: (e) => {
 		const coordinate = e.target.getAttribute('data-coordinate');
 		return coordinate;
 	},
 	setShip: (e) => {
 		const maxShip = 8;
-		let shipList = loadGame.playerOneShips;
-		let coordinate = loadGame.getCoordinate(e);
+		let shipList = battleShipt.playerOneShips;
+		let coordinate = battleShipt.getCoordinate(e);
 		let setCoordinate = e.target;
 		let shipsLen = shipList.length;
-		let duplicate = loadGame.matchCoordinate(shipList, coordinate);
+		let duplicate = battleShipt.matchCoordinate(shipList, coordinate);
 
-		if (shipsLen >= maxShip ) {
+		if (shipsLen >= maxShip) {
 			return console.log('reach max ship = ' + maxShip)
 		}
-		if ( coordinate === null ) {
+		if (coordinate === null) {
 			return
 		}
-		if (shipsLen === 0 || !duplicate ) {
+		if (shipsLen === 0 || !duplicate) {
 			// add ship icon to board
-			let setShip = loadGame.createElement('img', ['ship']);
+			let setShip = battleShipt.createElement('img', ['ship']);
 			setShip.setAttribute('src', './img/battleship.png');
 			setShip.setAttribute('alt', 'battleship icon');
-			shipList.push(coordinate)
 
 			// add coordinate to list
-			let shipCoord = loadGame.createElement('li', ['list-group-item']);
+			let shipCoord = battleShipt.createElement('li', ['list-group-item']);
 			shipCoord.innerText = `Ship ${shipsLen + 1} : row, column ${coordinate}`;
 
 			// add elements to dom
 			setCoordinate.appendChild(setShip);
-			loadGame.dashboard.appendChild(shipCoord);
+			battleShipt.dashboard.appendChild(shipCoord);
 
 			// push ship coordinate to Player List
-
+			shipList.push(coordinate)
 		} else {
 			console.log('select another position');
 		}
 	},
+	randomCoordinate: () => {
+
+	},
 	fireMissile: () => {
+		let currentPlaer = this.playerTurn;
 
 	},
 	sinkShip: (coord) => {
@@ -109,7 +137,10 @@ loadGame = {
 		itemContainer.addEventListener("transitionend", (e) => {
 			itemParent.removeChild(itemContainer);
 		}, false);
+	},
+	winner: () => {
+
 	}
 };
 
-loadGame.init();
+battleShipt.init();
